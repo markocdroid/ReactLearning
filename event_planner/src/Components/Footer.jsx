3c5fb767df1188d4de69fac032345283c59ce553
markocdroid <markoc.dev@gmail.com>
2026-01-02 09:33:45 -0500

    Finished practice exercise

---
+++ event_planner/src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <div>
+                    <p>By Marko C.</p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
