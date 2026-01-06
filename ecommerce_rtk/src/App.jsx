9a9466c8d9e0ec2bc85461427553d33dfe692e60
markocdroid <markoc.dev@gmail.com>
2026-01-06 18:35:29 -0500

    Finished the practice task.

--- ecommerce_rtk/src/App.jsx
+++ ecommerce_rtk/src/App.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 import ProductList from './Components/ProductList';
 import ShoppingCart from './Components/ShoppingCart';
 import './App.css'
+import SuperCoin from './Components/SuperCoin';
 const App = () => {
-  return (
- 
-      <div>
-        <h1 className='app-heading'>E-Commerce Application</h1>
-        <ProductList />
-        <ShoppingCart />
-      </div>
+    return (
 
-  );
+        <div>
+            <h1 className='app-heading'>E-Commerce Application</h1>
+            <ProductList />
+            <ShoppingCart />
+            <SuperCoin />
+        </div>
+
+    );
 };
 
 export default App;
